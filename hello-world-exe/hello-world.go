3e188af5933fbb7f78879909f6c9bc416c92db63
fyxme <jules.rigaudie@gmail.com>
2023-04-26 17:08:18 -0400

    added proxy stuff

---
+++ hello-world-exe/hello-world.go
@@ -0,0 +1,12 @@
+package main
+
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("This is an unsigned exe binary.")
+	log.Println("Not blocked by any security tools, nor applocker policies or equivalent.")
+
+}
